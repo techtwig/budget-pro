2cc40d75e1dcb9d9088fd3aabce52132ec92a54f
AfrarJahin <afrarjahin@gmail.com>
2023-11-13 10:54:48 +0600

    swagger setup

--- api-server-nestjs/src/main.ts
+++ api-server-nestjs/src/main.ts
@@ -1,14 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 /*import { config } from 'dotenv';
 
 
-config(); // Load environment variables from .env file*/
+// config(); // Load environment variables from .env file*/
 const PORT = 5000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+      .setTitle('Your API Title')
+      .setDescription('Your API description')
+      .setVersion('1.0')
+      .addServer('http://localhost:5000/', 'Local environment')
+      .addTag('Your API Tag')
+      .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(PORT);
