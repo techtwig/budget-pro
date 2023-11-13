9fcaad1cbfa86e9b51da21f94499466c9397c324
AfrarJahin <afrarjahin@gmail.com>
2023-11-13 11:48:21 +0600

    fix swagger setup

--- api-server-nestjs/src/main.ts
+++ api-server-nestjs/src/main.ts
@@ -11,13 +11,13 @@ const PORT = 5000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const options = new DocumentBuilder()
-      .setTitle('Budget pro')
-      .setDescription('Budget pro API description')
+      .setTitle('Your API Title')
+      .setDescription('Your API description')
       .setVersion('1.0')
-      .addServer('http://localhost:5000/', 'Local environment')
-      .addTag('API Tag')
-      .addBearerAuth(undefined, 'Apikey')
-      .addBearerAuth(undefined, 'Authorization')
+      .addServer('http://localhost:3000/', 'Local environment')
+      .addServer('https://staging.yourapi.com/', 'Staging')
+      .addServer('https://production.yourapi.com/', 'Production')
+      .addTag('Your API Tag')
       .build();
 
   const document = SwaggerModule.createDocument(app, options);
